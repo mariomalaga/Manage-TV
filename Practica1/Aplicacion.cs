149e62b93ab9ced9b87180808db080dcb58388fc
Mario Olivera <mariomalaga94@gmail.com>
2020-01-10 14:56:25 +0000

    TV project

---
+++ Practica1/Aplicacion.cs
@@ -0,0 +1,229 @@
+﻿/*
+* PRÁCTICA.............: Práctica 1.
+* NOMBRE y APELLIDOS...: Mario Olivera Castañeda
+* CURSO y GRUPO........: 2o Desarrollo de Interfaces
+* TÍTULO de la PRÁCTICA: P.O.O. Abstracción. Definición de Clases.
+* FECHA de ENTREGA.....: 17 de Octubre de 2018
+*/
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Aplicacion
+    {
+        #region Variables
+        private static Tv tv;
+        #endregion
+        #region Main
+        static void Main(string[] args)
+        {
+            string casos = "";
+            tv = new Tv();
+            Elegir();
+            Console.WriteLine("Compramos una " + tv.Marca + "...");
+            tv = new Tv(tv.Marca, tv.Pulgadas, tv.Consumo, tv.Precio);
+            while (casos != "8")
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Caso 1: Información técnica de la televisión.");
+                Console.WriteLine("Caso 2: Encender o apagar la televisión.");
+                Console.WriteLine("Caso 3: Subir el volúmen de la televisión.");
+                Console.WriteLine("Caso 4: Bajar el volúmen de la televisión.");
+                Console.WriteLine("Caso 5: Poner canal elegido.");
+                Console.WriteLine("Caso 6: Cambiar canal anterior.");
+                Console.WriteLine("Caso 7: Estado de la televisión.");
+                Console.WriteLine("Caso 8: Salir");
+                Console.WriteLine(" ");
+                Console.ForegroundColor = ConsoleColor.White;
+                casos = Console.ReadLine();
+                #region Switch
+                switch (casos)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Información técnica de la televisión: \n");
+                        tv.InformacionTecnica();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        tv.PulsarOnOff();
+                        break;
+                    case "3":
+                        Console.Clear();
+                        tv.SubirVolumen();
+                        break;
+                    case "4":
+                        Console.Clear();
+                        tv.BajarVolumen();
+                        break;
+                    case "5":
+                        Console.Clear();
+                        int ponerCanal = ControlarEntradaInt("Introducir canal: "); 
+                        tv.PonerCanal(ponerCanal);
+                        break;
+                    case "6":
+                        Console.Clear();
+                        tv.CambiarCanalAnterior();
+                        break;
+                    case "7":
+                        Console.Clear();
+                        tv.EstadoActual();
+                        break;
+                    case "8":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("Fin de la ejecución.");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Has introducido un valor distinto a los indicados.");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Por favor introduzca un valor de los indicados.\n");
+                        break;
+                }
+                #endregion
+            }
+        }
+        #endregion
+        #region Metodos
+        #region Metodo Elegir
+        static void Elegir()
+        {
+        string eleccion;
+            bool eleccionCorrecto = false;
+            while (!eleccionCorrecto)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Escriba 1 para poner valores de televisión.");
+                Console.WriteLine("Escriba 2 para usar valores por defecto de televisión.\n");
+                eleccion = Console.ReadLine(); 
+                switch (eleccion)
+                {
+                    case "1":
+                        bool introducirPulgadas = false;
+                        bool introducirConsumo = false;
+                        bool introducirPrecio = false;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Introduzca marca del televisor");
+                        tv.Marca = Console.ReadLine();
+                        tv.Pulgadas = ControlarEntradaFloat("Introduzca pulgadas del televisor (entre 11 y 55 pulgadas)");
+                        while (!introducirPulgadas)
+                        {
+                            Console.Clear();
+                            if (tv.Pulgadas < 11 || tv.Pulgadas > 55)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write("Error al introducir las pulgadas\n");
+                                tv.Pulgadas = ControlarEntradaFloat("Introduzca pulgadas del televisor (entre 11 y 55 pulgadas)");
+                            }
+                            else
+                            {
+                                introducirPulgadas = !introducirPulgadas;
+                            }
+                        }
+                        tv.Consumo = ControlarEntradaFloat("Introduzca consumo del televisor (entre 25 y 300)");
+                        while (!introducirConsumo)
+                        {
+                            Console.Clear();
+                            if (tv.Consumo < 25 || tv.Consumo >= 300)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write("Error al introducir el consumo\n");
+                                tv.Consumo = ControlarEntradaFloat("Introduzca consumo del televisor (entre 25 y 300)");
+                            }
+                            else
+                            {
+                                introducirConsumo = !introducirConsumo;
+                            }
+                        }
+                        tv.Precio = ControlarEntradaFloat("Introduzca precio del televisor (entre 100 y 10000)\n");
+                        while (!introducirPrecio)
+                        {
+                            Console.Clear();
+                            if (tv.Precio < 100 || tv.Precio > 10000)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write("Error al introducir el precio\n");
+                                tv.Precio = ControlarEntradaFloat("Introduzca precio del televisor (entre 100 y 10000) ");
+                            }
+                            else
+                            {
+                                introducirPrecio = !introducirPrecio;
+                            }
+                        }
+                        eleccionCorrecto = !eleccionCorrecto;
+                        break;
+                    case "2":
+                        tv.Marca = "Sanyo";
+                        tv.Pulgadas = 28;
+                        tv.Consumo = 300;
+                        tv.Precio = 499.95f;
+                        eleccionCorrecto = !eleccionCorrecto;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Clear();
+                        Console.WriteLine("Has introducido un valor distinto a los indicados.");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Por favor introduzca un valor de los indicados.\n");
+                        break;
+                }
+            }
+        }
+        #endregion
+        #region Metodo ControlarEntradaFloat
+        static float ControlarEntradaFloat(string texto)
+        {
+            float valor;
+            bool esNumero;
+            string variable;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(texto + "\n");
+                variable = Console.ReadLine();
+                esNumero = float.TryParse(variable, out valor);
+                if (!esNumero)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error al introducir. Por favor vuelva a introducir\n");
+                }
+            }
+            while (!esNumero);
+            return float.Parse(variable);
+        }
+        #endregion
+        #region Metodo ControlarEntradaInt
+        static int ControlarEntradaInt(string texto)
+        {
+            int valor;
+            bool esNumero;
+            string variable;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(texto + "\n");
+                variable = Console.ReadLine();
+                esNumero = int.TryParse(variable, out valor);
+                if (!esNumero)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error al introducir. Por favor vuelva a introducir\n");
+                }
+            }
+            while (!esNumero);
+            return int.Parse(variable);
+        }
+        #endregion
+        #endregion
+    }
+}
---
+++ Practica1/Tv.cs
@@ -0,0 +1,202 @@
+﻿/*
+* PRÁCTICA.............: Práctica 1.
+* NOMBRE y APELLIDOS...: Mario Olivera Castañeda
+* CURSO y GRUPO........: 2o Desarrollo de Interfaces
+* TÍTULO de la PRÁCTICA: P.O.O. Abstracción. Definición de Clases.
+* FECHA de ENTREGA.....: 17 de Octubre de 2018
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Tv
+    {
+        #region Variables
+        private string marca;
+        private float pulgadas;
+        private float consumo;
+        private float precio;
+        private bool onOff = true;
+        private int canal;
+        private int canalAnterior;
+        private int volumen;
+        #endregion
+        #region Constructor
+        public Tv(string marca, float pulgadas, float consumo, float precio)
+        {
+            this.Marca = marca;
+            this.Pulgadas = pulgadas;
+            this.Consumo = consumo;
+            this.Precio = precio;
+            canal = 0;
+            canalAnterior = 0;
+            volumen = 1;
+        }
+        public Tv()
+        {
+
+        }
+        #endregion
+        #region Metodos
+        #region Metodo PulsarOnOff
+        public void PulsarOnOff()
+        {
+            if (!onOff)
+            {
+                onOff = !onOff;
+                canal = 0;
+                canalAnterior = 0;
+                volumen = 1;
+                EstadoActual();  
+            }
+            else 
+            {
+                onOff = !onOff;
+                canal = 1;
+                canalAnterior = 1;
+                volumen = 25;
+                EstadoActual();
+            }
+        }
+        #endregion
+        #region Metodo SubirVolumen
+        public void SubirVolumen()
+        {
+            if (!onOff)
+            {
+                if (volumen < 100 && volumen >= 1)
+                {
+                    volumen++;
+                    EstadoActual();
+                }
+                else if (volumen == 100)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Volumen al máximo no se puede subir más el volumen");
+                    Console.Beep();
+                    EstadoActual();
+                }
+            }else {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Beep();
+                }
+                EstadoActual();
+            }
+        }
+        #endregion
+        #region Metodo BajarVolumen
+        public void BajarVolumen()
+        {
+            if (!onOff)
+            {
+                if (volumen <=100 && volumen > 1)
+                {
+                    volumen--;
+                    EstadoActual();
+                }
+                else if(volumen == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Volumen al mínimo, no se puede bajar más el volumen");
+                    Console.Beep();
+                    EstadoActual();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Beep();
+                }
+                EstadoActual();
+            }
+        }
+        #endregion
+        #region Metodo PonerCanal
+        public void PonerCanal(int nuevoCanal)
+        {
+            if (!onOff)
+            {
+                if (nuevoCanal > 99 || nuevoCanal < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error al introducir el canal");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Por favor introduzca un canal entre 0 y 100");
+                    Console.Beep();
+                    EstadoActual();
+                }
+                else
+                {
+                    canalAnterior = canal;
+                    canal = nuevoCanal;
+                    EstadoActual();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Beep();
+                }
+                EstadoActual();
+            }
+        }
+        #endregion
+        #region Metodo CambiarCanalAnterior
+        public void CambiarCanalAnterior()
+        {
+            int guardarCanal;
+            if (!onOff)
+            {
+                guardarCanal = canal;
+                canal = canalAnterior;
+                canalAnterior = guardarCanal;
+                EstadoActual();
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Beep();
+                }
+                EstadoActual();
+            }
+        }
+        #endregion
+        #region Metodo EstadoActual
+        public void EstadoActual()
+        {
+            if (!onOff)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Encendida->Canal: "+canal+", Volumen: "+volumen);
+            }else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Apagada!");
+            }
+        }
+        #endregion
+        #region Metodo InformacionTecnica
+        public void InformacionTecnica()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Marca: "+Marca+". Tamaño en pulgadas: "+Pulgadas+". Consumo: "+Consumo+"W. Precio: "+Precio+" Euros.\n");
+        }
+        #endregion
+        #region Propiedades
+        public string Marca { get => marca; set => marca = value; }
+        public float Pulgadas { get => pulgadas; set => pulgadas = value; }
+        public float Consumo { get => consumo; set => consumo = value; }
+        public float Precio { get => precio; set => precio = value; }
+        #endregion
+        #endregion
+    }
+}
+
